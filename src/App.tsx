5f2f20ee233fb63ff89a6b18589460334c3a3af6
[dyad] <omaralymamudo@gmail.com>
2025-10-28 15:46:38 +0000

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import AppLayout from "./components/AppLayout"; // Importando AppLayout
 
 const queryClient = new QueryClient();
 
@@ -15,8 +16,15 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* Rota de Layout Principal */}
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Index />} /> {/* Rota padrão (/) */}
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            
+            {/* Rotas que não usam o layout (se houver) podem ser adicionadas fora do AppLayout, mas por enquanto, todas usam. */}
+          </Route>
+          
+          {/* Rota 404 deve estar fora do AppLayout para não mostrar o header/footer em caso de erro */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
