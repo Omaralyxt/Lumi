e4b5e315c2bd05b0076eae72bf377243f0dd4d42
[dyad] <omaralymamudo@gmail.com>
2025-10-29 03:22:23 +0000

    [dyad] Removed the menu button from the Home page navigation. - wrote 1 file(s)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -203,11 +203,9 @@ export default function Home() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         
         {/* Top Navigation & Search */}
-        <div className="mb-4"> {/* Adicionando margem inferior para separar da próxima seção */}
+        <div className="mb-4">
           <div className="flex items-center justify-between mb-4">
-            <Button variant="ghost" size="icon" onClick={() => navigate('/menu')}>
-              <Menu className="h-6 w-6 text-gray-700 dark:text-gray-300" />
-            </Button>
+            {/* Menu Button Removed */}
             
             <h1 className="text-2xl font-bold text-blue-600 dark:text-blue-400">Lumi</h1>
 
@@ -230,7 +228,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="relative mb-4"> {/* Adicionando margem inferior para separar da Separator */}
+          <div className="relative mb-4">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
             <Input
               type="text"
